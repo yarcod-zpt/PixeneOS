1923f7270805c5260b0f7629e25a1da1b495b8be
Daniel Edholm <daniel.edholm@zptcorp.com>
2025-08-05 00:39:08 +0200

    fix: Use local gh username for ota destination

--- src/declarations.sh
+++ src/declarations.sh
@@ -23,7 +23,7 @@ WORKDIR=".tmp"
 # GitHub variables
 DOMAIN="https://github.com"
 REPOSITORY="PixeneOS" # GitHub repository name
-USER="pixincreate"    # GitHub username
+USER="yarcod"         # GitHub username
 
 # Application version variables
 VERSION[AFSR]="${VERSION[AFSR]:-1.0.3}"
